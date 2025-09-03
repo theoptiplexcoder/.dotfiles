65fa5be032617cd9043f24724dd634906d24078f
Your Name <you@example.com>
2025-09-02 20:37:00 -0400

    lsp.bak

--- nvim/.config/nvim/lua/mr_optiplex/plugins/lsp.lua
+++ nvim/.config/nvim/lua/mr_optiplex/plugins/lsp.lua
@@ -14,7 +14,23 @@ return {
       local lspconfig = require('lspconfig')
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      -- Set up cmp (you can customize this further)
+      -- LSP keymaps for attached buffers
+      local on_attach = function(_, bufnr)
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+
+        -- Navigation
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+        -- Diagnostics
+        vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+      end
+
+      -- Setup cmp
       cmp.setup({
         snippet = {
           expand = function(args)
@@ -23,41 +39,82 @@ return {
         },
         mapping = cmp.mapping.preset.insert({
           ['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true}),
-	   ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-	   ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
-	   ['<C-p>'] = cmp.mapping.select_next_item(),
-           ['<C-n>'] = cmp.mapping.select_prev_item(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
         }),
         sources = {
-          { name = 'luasnip'},
-	  {name = 'nvim_lsp'},
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
         },
       })
 
-      -- Configure LSP servers
-      local servers = { "lua_ls","vimls"}
-      for _, lsp in ipairs(servers) do
-      	local ok, server = pcall(function() return lspconfig[lsp] end)
-	if ok then
-		server.setup{capabilities = capabilities}
+      -- Configure lua_ls with Neovim runtime
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            telemetry = { enable = false },
+          },
+        },
+      })
 
-	end
+      -- Configure pylsp (Python)
+      lspconfig.pylsp.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = {
+          pylsp = {
+            plugins = {
+              pyflakes = { enabled = true },
+              pycodestyle = { enabled = true },
+              pylint = { enabled = true },
+              mccabe = { enabled = true },
+              jedi_completion = { fuzzy = true },
+            },
+          },
+        },
+      })
+
+      -- Other servers with default config
+      local servers = { "vimls", "quick_lint_js" }
+      for _, lsp in ipairs(servers) do
+        local ok, server = pcall(function() return lspconfig[lsp] end)
+        if ok then
+          server.setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+          })
+        end
       end
     end,
   },
-	{
-		"williamboman/mason.nvim",
-  		dependencies = {
-  		  "williamboman/mason-lspconfig.nvim",
-  		  "neovim/nvim-lspconfig",
-  		},
-  		config = function()
-  		  require("mason").setup()
-  		  require("mason-lspconfig").setup({
-  		  ensure_installed = {"lua_ls","vimls","quick_lint_js"}, -- auto-installs these
-  		  })
-  		end,
-	}
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "vimls",
+          "quick_lint_js",
+          "pylsp", -- ✅ added pylsp so it installs automatically
+        },
+      })
+    end,
+  }
 }
 
